73a198028b923d03285b568f50c8864a51bfe5ea
Lenore Kelly <91968585+LenoreK@users.noreply.github.com>
2022-02-20 16:25:14 -0500

    These is problem with the connection between city in index.js and places.js

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src='../images/Cake.jpg' alt='chocolate cake' />
+                <div>
+                  Photo by <a href='https://www.pexels.com/@abhinavcoca'>Abhinav Goswami</a> on <a href='https://www.pexels.com/'>Pixels</a>
+                </div>
+              </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,9 +4,15 @@ const Def = require('../default.jsx')
 function Index (data) {
   let placesFormatted = data.places.map((place) => {
     return (
-      <div>
+      <div className='col-sm-6'>
         <h2>{place.name}</h2>
+        <p className='text-center'>
+          {place.cuisines}
+        </p>
         <img src={place.pic} alt={place.name}/>
+        <p className='text-center'>
+          Located in {place,city}, {place,state}
+        </p>
       </div>
     )
   })
@@ -14,7 +20,9 @@ function Index (data) {
     <Def>
         <main>
             <h1>PLACES INDEX PAGE</h1>
+            <div className='row'>
             {placesFormatted}
+            </div>
         </main>
     </Def>
 )
