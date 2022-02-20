bb3723d9348dc59c600e781a3c5d9a3104d37cdb
Lenore Kelly <91968585+LenoreK@users.noreply.github.com>
2022-02-20 11:38:57 -0500

    Added .env file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Hello world!')
+require('dotenv').config()
+ 
+app.get('/', function (req, res) {
+    res.send('Hello World!')
 })
-
-app.listen(3000)
+ 
+app.listen(process.env.PORT)
