e95dc88bb1c4f71fbd49ad733320edebe6f27d63
Lenore Kelly <91968585+LenoreK@users.noreply.github.com>
2022-02-23 14:09:20 -0500

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -24,6 +24,20 @@ router.get('/:id', (req, res) => {
   res.render('places/show', { place: places[id] })
 })
 
+//Edit Page
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 router.post('/', (req, res) => {
   console.log(req.body)
   if (!req.body.pic) {
