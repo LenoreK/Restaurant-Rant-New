4dbc6b51473b9f50ea8c883e0cfb2b56dfbceecb
Lenore Kelly <91968585+LenoreK@users.noreply.github.com>
2022-03-01 13:47:09 -0500

    Added moongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,82 @@
 const router = require('express').Router()
-const places = require('../models/places.js')
+const db = require('../models')
+//const places = require('../models/places.js')
 
+//INDEX GET ROUTE
 router.get('/', (req, res) => {
+    db.Place.find()
+    .then((places) => {
+      res.render('places/index', { places })
+    })
+    .catch(err => {
+      console.log(err) 
+      res.render('error404')
+    })
+})
+
+/*router.get('/', (req, res) => {
+  res.send('GET /places stub')
+})*/
+
+//INDEX POST ROUTE
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+/*router.post('/', (req, res) => {
+  res.send('POST /places stub')
+})*/
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+//ID ROUTE
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+/*router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})*/
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+/*router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
@@ -95,6 +170,4 @@ router.delete('/places/:id', (req, res) => {
     places.splice(id, 1)
     res.redirect('/places')
   }
-})
-
-module.exports = router
+})*/
